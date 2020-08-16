29774782cf991f6ca399aa61d9b1523a37f3e863
Dennis Padiernos <depadiernos@gmail.com>
2020-08-16 18:55:38 -0500

    ex1 completed

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,4 +4,14 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     # Your code here
 
+    cache = {}
+
+    for index, weight in enumerate(weights):
+        cache[weight] = index
+
+    for index, weight in enumerate(weights):
+            diff = limit - weight
+            if diff in cache:
+                return (max(index, cache[diff]), min(index, cache[diff]))
+
     return None
